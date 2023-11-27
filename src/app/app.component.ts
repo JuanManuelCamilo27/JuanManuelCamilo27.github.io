0c534000fab828badbeab0559f6811315fc396a2
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2023-11-27 12:19:37 -0300

    Inicio base del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,22 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { NavComponent } from './components/nav/nav.component';
+import { MainComponent } from './components/main/main.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavComponent,
+    MainComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'games-page';
