c336f5bd54b9568ed7d3b9b30f95a394790e3a5c
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2024-01-23 22:05:11 -0300

    Created the board component and imported it to the memory component

--- src/app/components/memory/memory.component.ts
+++ src/app/components/memory/memory.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BoardComponent } from './board/board.component';
 
 @Component({
   selector: 'app-memory',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BoardComponent],
   templateUrl: './memory.component.html',
-  styleUrl: './memory.component.scss'
+  styleUrl: './memory.component.scss',
 })
-export class MemoryComponent {
-
-}
+export class MemoryComponent {}
