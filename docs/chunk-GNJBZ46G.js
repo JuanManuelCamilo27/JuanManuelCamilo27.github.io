58bebdb2a39a6345f85eb3d216c3a556c615bccf
Juan Manuel Camilo <juanma.camilo.27@gmail.com>
2024-02-14 21:21:05 -0300

    Build

--- docs/chunk-GNJBZ46G.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-LXGAOE32.js";import"./chunk-D4KGBPOG.js";var n=[{path:"",component:o}];export{n as routes};
---
+++ docs/chunk-PSRGZ3XN.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-7R5ATLKK.js";import"./chunk-D4KGBPOG.js";var n=[{path:"",component:o}];export{n as routes};
